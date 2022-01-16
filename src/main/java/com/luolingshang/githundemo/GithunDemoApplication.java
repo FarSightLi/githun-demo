c53419a29218e30ff2108d02270461853be0d802
FaiSightLi <farsight@live.com>
2022-01-16 11:06:31 +0800

    test

---
+++ src/main/java/com/luolingshang/githundemo/GithunDemoApplication.java
@@ -0,0 +1,13 @@
+package com.luolingshang.githundemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithunDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithunDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luolingshang/githundemo/GithunDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luolingshang.githundemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithunDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
